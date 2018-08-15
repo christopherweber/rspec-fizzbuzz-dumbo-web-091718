0f23643364770c83d4187e204374b291fe6907df
Christopher Weber <chrisrrweber@gmail.com>
2018-08-15 23:08:28 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,9 +2,9 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0 
+  if int % 3 === 0 && int % 5 === 0
     "Fizz" 
-  elsif int % 3 === 0 && int % 5 === 0
+  elsif int % 3 === 0
     "FizzBuzz"
   elsif int % 5
     "Buzz"
